9eda57e03e302fbe56363d8f306094eb167a74a9
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-05-18 17:22:46 -0300

    fix: adjust form validations

--- src/schemas/contact.schemas.ts
+++ src/schemas/contact.schemas.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const formContactSchema = yup.object().shape({
-    name: yup.string().required("Nome obrigatório").max(50, "Deve ter no máximo 50 caracteres"),
+    name: yup.string().required("Nome obrigatório").max(16, "Deve ter no máximo 16 caracteres"),
     email: yup.string().required("Email obrigatório").email("Email inválido"),
     cell_phone: yup
     .string()
@@ -9,5 +9,5 @@ export const formContactSchema = yup.object().shape({
     .matches(/^\(\d{2}\)\s\d{5}-\d{4}$/, "Formato de telefone inválido Ex: (99) 99999-9999")
     .max(15, "Deve ter no máximo 15 caracteres")
     .min(15, "Deve ter no mínimo 15 caracteres"),
-    description: yup.string().max(30, "Deve ter no máximo 30 caracteres")
+    description: yup.string().max(24, "Deve ter no máximo 30 caracteres")
 })
